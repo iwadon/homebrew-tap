1e3f91b5f47f2f30f557e7a182353ca4cd90c211
IWATSUKI Hiroyuki <don@na.rim.or.jp>
2026-06-06 23:21:10 +0900

    fix(terse): descを修正し、文末のピリオドを削除

--- Formula/terse-prompt.rb
+++ Formula/terse-prompt.rb
@@ -1,5 +1,5 @@
 class TersePrompt < Formula
-  desc "A lightweight, modern C library for multi-line text input in CLI applications."
+  desc "Lightweight, modern C library for multi-line text input in CLI applications"
   homepage "https://github.com/iwadon/terse-prompt"
   license "MIT-0"
   head "https://github.com/iwadon/terse-prompt.git", branch: "master"
@@ -8,7 +8,12 @@ class TersePrompt < Formula
   depends_on "terse"
 
   def install
-    system "cmake", "-S", ".", "-B", "build", "-DTPROMPT_BUILD_TESTING=OFF", "-DTPROMPT_BUILD_EXAMPLES=OFF", "-DTPROMPT_USE_TERSE_TEST_MODE=OFF", *std_cmake_args
+    configure_args = std_cmake_args + %w[
+      -DTPROMPT_BUILD_TESTING=OFF
+      -DTPROMPT_BUILD_EXAMPLES=OFF
+      -DTPROMPT_USE_TERSE_TEST_MODE=OFF
+    ]
+    system "cmake", "-S", ".", "-B", "build", *configure_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
   end
