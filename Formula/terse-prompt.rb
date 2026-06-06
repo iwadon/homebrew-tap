6ace6d6a1cca01254bd782ef6ba9c50dd1cf5cdf
IWATSUKI Hiroyuki <don@na.rim.or.jp>
2026-06-06 23:33:31 +0900

    feat: terse-promptにテストを追加

--- Formula/terse-prompt.rb
+++ Formula/terse-prompt.rb
@@ -17,4 +17,26 @@ class TersePrompt < Formula
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
   end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <tprompt.h>
+      int main(void) {
+        terse_handle_t terse_handle = terse_open(TERSE_PROFILE_AUTO, NULL);
+        if (!terse_handle) return 1;
+        tprompt_options_t options = TPROMPT_OPTIONS_DEFAULT;
+        options.terse_handle = terse_handle;
+        tprompt_handle_t handle = tprompt_open(&options);
+        if (!handle) {
+          terse_close(terse_handle);
+          return 1;
+        }
+        tprompt_close(handle);
+        terse_close(terse_handle);
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-ltprompt", "-L#{Formula["terse"].opt_lib}", "-lterse", "-liconv", "-o", "test"
+    system "./test"
+  end
 end
